50c8866b1e345ed3447660b64edf7703f433edd9
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2021-01-31 04:37:15 +0900

    Create Project boj-breaker

---
+++ src/main/java/ddbreaker/bojbreaker/BojBreakerApplication.java
@@ -0,0 +1,13 @@
+package ddbreaker.bojbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BojBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BojBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddbreaker/bojbreaker/BojBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package ddbreaker.bojbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BojBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
