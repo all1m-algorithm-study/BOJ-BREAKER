81de267e058419626494ffadc4c7bc0541c11f88
kir3i <kirei7081@gmail.com>
2021-01-31 18:00:36 +0900

    add Problem Entity Class

---
+++ src/main/java/ddbreaker/bojbreaker/domain/problem/Problem.java
@@ -0,0 +1,38 @@
+package ddbreaker.bojbreaker.domain.problem;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Problem {
+
+    @Id
+    private Long id;            //문제 번호
+
+    @Column(nullable = false)
+    private String title;       //문제 이름
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SolvedAcTier tier;
+
+    private Long acTries;       // 맞은 사람 수
+    private double avgTries;    // 평균 시도 횟수
+
+//    private boolean isSolved;
+    // School에 관련 값이 들어가야 하는지 체크 요함
+
+    @Builder
+    public Problem(Long id, String title, SolvedAcTier tier, Long acTries, double avgTries) {
+        this.id = id;
+        this.title = title;
+        this.tier = tier;
+        this.acTries = acTries;
+        this.avgTries = avgTries;
+    }
+}
