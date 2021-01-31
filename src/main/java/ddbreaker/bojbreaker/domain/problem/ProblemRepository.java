3c6ea88e377ad03b8c7c7c2fc84344c1d232deef
kir3i <kirei7081@gmail.com>
2021-01-31 18:03:42 +0900

    add ProblemRepository

---
+++ src/main/java/ddbreaker/bojbreaker/domain/problem/ProblemRepository.java
@@ -0,0 +1,6 @@
+package ddbreaker.bojbreaker.domain.problem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemRepository extends JpaRepository<Problem, Long> {
+}
