fcbaca6193ce4b4cc49fb66915450ea12f071b54
kir3i <kirei7081@gmail.com>
2021-02-02 02:58:06 +0900

    crawlProblems 함수 구현

--- src/main/java/ddbreaker/bojbreaker/service/Crawler.java
+++ src/main/java/ddbreaker/bojbreaker/service/Crawler.java
@@ -1,12 +1,23 @@
 package ddbreaker.bojbreaker.service;
 
+import ddbreaker.bojbreaker.domain.problem.SolvedAcTier;
 import ddbreaker.bojbreaker.domain.solvedLogs.SolvedLogsRepository;
+import ddbreaker.bojbreaker.service.dto.ProblemParseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.ExtensionMethod;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
 public class Crawler {
 
     public Long updateSolvedProlems() {
@@ -31,5 +42,40 @@ public class Crawler {
             System.out.println("jsoup error");
         }
         return 0L;
+    }
+
+    // solved.ac에서 각 티어별 문제 parsing
+    public List<ProblemParseDto> crawlProblmes(SolvedAcTier tier) throws Exception {
+        List<ProblemParseDto> parsedProblems = new ArrayList<>();
+        String uri = "https://solved.ac/problems/level/" + tier.ordinal();
+        for (int page = 1 ; ; page++) {
+            Document document = Jsoup.connect(uri+"?page="+page)
+                    .timeout(5000)
+                    .get();
+            System.out.println(uri+"?page="+page);
+            Elements problemIds = document.select(".ProblemInline__ProblemStyle-cvf1lm-0");
+            Elements problemTitles = document.select(".hover_underline");
+            Elements acTriesAndAvgTries = document.select(".TabularFigures__TabularFiguresStyle-ved8ab-0");
+
+            int numOfProblmes = problemIds.size();
+
+            for (int i = 0; i < numOfProblmes; i++) {
+                parsedProblems.add(
+                    ProblemParseDto.builder()
+                            .id(Long.parseLong(problemIds.get(i).text()))
+                            .title(problemTitles.get(i).text())
+                            .acTries(Long.parseLong(acTriesAndAvgTries.get(2*i).text().replaceAll(",", "")))
+                            .avgTries(Double.parseDouble(acTriesAndAvgTries.get(2*i+1).text()))
+                            .tier(tier)
+                            .build()
+                );
+            }
+            
+            if (problemIds.size() < 100)
+                break;
+            Thread.sleep((int)(Math.random() * 10000) + 7000);   // 7~10초 대기
+        }
+
+        return parsedProblems;
     }
 }
