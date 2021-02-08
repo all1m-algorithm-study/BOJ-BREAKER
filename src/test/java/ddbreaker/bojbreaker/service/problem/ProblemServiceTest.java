11577983407a95fb78979a1df458c28d7898ea37
kir3i <kirei7081@gmail.com>
2021-02-09 00:03:47 +0900

    ProblemService 테스트 작성

---
+++ src/test/java/ddbreaker/bojbreaker/service/problem/ProblemServiceTest.java
@@ -0,0 +1,47 @@
+package ddbreaker.bojbreaker.service.problem;
+
+import ddbreaker.bojbreaker.domain.problem.Problem;
+import ddbreaker.bojbreaker.domain.problem.ProblemRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ProblemServiceTest {
+
+    ProblemService problemService;
+    ProblemRepository problemRepository;
+
+    @Autowired
+    public ProblemServiceTest(ProblemService problemService, ProblemRepository problemRepository) {
+        this.problemService = problemService;
+        this.problemRepository = problemRepository;
+    }
+
+    @AfterEach
+    public void cleanup() {
+
+    }
+    @Test
+    public void 전체_문제_크롤링_테스트() {
+        //given
+
+        //when
+        problemService.updateAllProblems();
+
+        //then
+        List<Problem> allProblems = problemRepository.findAll();
+        assertThat(allProblems.size()).isGreaterThan(1);
+        for (Problem p: allProblems)
+            System.out.println(p.getId() + ' ' + p.getTitle() + ' ' + p.getTier());
+
+    }
+}
