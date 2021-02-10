aa83b0cf487dd3e2a7c9041636822184881d647f
kir3i <kirei7081@gmail.com>
2021-02-10 20:07:50 +0900

    findByProblemId() 추가

--- src/main/java/ddbreaker/bojbreaker/domain/problem/ProblemRepository.java
+++ src/main/java/ddbreaker/bojbreaker/domain/problem/ProblemRepository.java
@@ -2,5 +2,9 @@ package ddbreaker.bojbreaker.domain.problem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+
+    Optional<Problem> findByProblemId(Long id);
 }
