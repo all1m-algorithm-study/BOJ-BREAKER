24d4669a9da905a14029d74ae4f0da4a3c1acd36
kir3i <kirei7081@gmail.com>
2021-02-10 22:21:43 +0900

    builder 추가

--- src/main/java/ddbreaker/bojbreaker/domain/solved/Solved.java
+++ src/main/java/ddbreaker/bojbreaker/domain/solved/Solved.java
@@ -2,6 +2,7 @@ package ddbreaker.bojbreaker.domain.solved;
 
 import ddbreaker.bojbreaker.domain.problem.Problem;
 import ddbreaker.bojbreaker.domain.school.School;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,4 +30,11 @@ public class Solved {
     private String solvedUser;      // 푼 유저 아이디
     private LocalDateTime solvedDate;       // 푼 시간
 
+    @Builder
+    public Solved(Problem problem, School school, String solvedUser, LocalDateTime solvedDate) {
+        this.problem = problem;
+        this.school = school;
+        this.solvedUser = solvedUser;
+        this.solvedDate = solvedDate;
+    }
 }
