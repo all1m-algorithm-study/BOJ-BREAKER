0ca615e0e14db054733354aa4708e6e1e7988fec
kir3i <kirei7081@gmail.com>
2021-02-10 20:07:13 +0900

    PK를 자동생성 key로 바꿈

--- src/main/java/ddbreaker/bojbreaker/domain/problem/Problem.java
+++ src/main/java/ddbreaker/bojbreaker/domain/problem/Problem.java
@@ -15,7 +15,11 @@ import java.util.Set;
 public class Problem {
 
     @Id
-    private Long problemId;            //문제 번호
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long problemId;     //문제 번호
 
     @Column(nullable = false)
     private String title;       //문제 이름
