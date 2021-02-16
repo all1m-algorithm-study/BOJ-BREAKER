739e53fc87aaf65378657e3880fe0a9501f5064f
kir3i <kirei7081@gmail.com>
2021-02-17 01:31:03 +0900

    App이 시작할 때 문제/학교 정보 크롤링 하도록 설정

---
+++ src/main/java/ddbreaker/bojbreaker/AppStartupRunner.java
@@ -0,0 +1,22 @@
+package ddbreaker.bojbreaker;
+
+import ddbreaker.bojbreaker.service.problem.ProblemService;
+import ddbreaker.bojbreaker.service.school.SchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AppStartupRunner implements ApplicationRunner {
+
+    private final ProblemService problemService;
+    private final SchoolService schoolService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        problemService.updateAllProblems(3);
+//        schoolService.register(302L);
+    }
+}
